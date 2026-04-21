51000551ab6b82f78fb0aa032cd282129ab790eb
JULIA DELAMURA BUFFALO <juju.delamurabuffalo@gmail.com>
2026-04-21 17:21:04 -0300

    Create analises_desafio.sql

---
+++ analises_desafio.sql
@@ -0,0 +1,11 @@
+SELECT * FROM Alunos;
+SELECT * FROM Disciplinas;
+SELECT * FROM Notas;
+SELECT * FROM Professores;
+SELECT * FROM Turmas;
+SELECT * FROM Turma_Alunos;
+SELECT * FROM Turma_Disciplinas;
+
+SELECT * FROM Alunos ORDER BY nome_aluno;
+SELECT * FROM Disciplinas WHERE carga_horaria > 40;
+SELECT * FROM Notas WHERE nota > 6 AND nota < 8;
